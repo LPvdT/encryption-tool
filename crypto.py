77104b3a6b2b703540fcdd5660f9341bc14cf5da
Laurens van der Tas <laurens.vandertas@gmail.com>
2021-11-22 21:18:32 +0100

    Change: Documentation; formatting; refactoring

--- crypto.py
+++ crypto.py
@@ -2,7 +2,6 @@ import click
 
 from typing import Union, Optional
 
-from src.encryption.crypto import CryptoEngine
 from src.encryption.decrypt import Decryptor
 from src.encryption.encrypt import Encryptor
 
@@ -30,7 +29,11 @@ def main(
     iterations: Union[int, float],
     salt: Optional[Union[int, float, str]] = None,
 ):
-    ctx.obj["console"] = CryptoEngine().CONSOLE
+    """
+    Simple tool to encrypt or decrypt your files and folders using the SHA-2 algorithm with 512-bit hashing.
+    """
+
+    # Add properties to context object
     ctx.obj["output_folder"] = output_folder
     ctx.obj["iterations"] = iterations
     ctx.obj["salt"] = salt
@@ -47,50 +50,57 @@ def main(
 @click.argument("targets", nargs=-1)
 @click.pass_context
 def encrypt(ctx, save_key: bool = False, **kwargs):
+    """
+    Encrypt file(s), or folder(s).
+    """
 
+    # Class object instance
     encryptor = Encryptor()
 
+    # Add property to context object
     ctx.obj["save_key"] = save_key
 
+    # Handlers for direct and indirect encryption targets
+    encrypt_params = {
+        "output_folder": ctx.obj["output_folder"],
+        "iterations": ctx.obj["iterations"],
+        "salt": ctx.obj["salt"],
+        "save_key": ctx.obj["save_key"],
+    }
+
     if len(kwargs["targets"]) > 0:
-        encryptor.encrypt_targets(
-            output_folder=ctx.obj["output_folder"],
-            iterations=ctx.obj["iterations"],
-            salt=ctx.obj["salt"],
-            save_key=ctx.obj["save_key"],
-            targets=kwargs["targets"],
-        )
+        encrypt_params.update(targets=kwargs["targets"])
+        encryptor.encrypt_targets(**encrypt_params)
     else:
-        encryptor.encrypt_targets(
-            output_folder=ctx.obj["output_folder"],
-            iterations=ctx.obj["iterations"],
-            salt=ctx.obj["salt"],
-            save_key=ctx.obj["save_key"],
-        )
+        encryptor.encrypt_targets(**encrypt_params)
 
 
 @main.command()
 @click.argument("targets", nargs=-1)
 @click.pass_context
 def decrypt(ctx, **kwargs):
+    """
+    Decrypt file(s), or folder(s).
+    """
 
+    # Class object instance
     decryptor = Decryptor()
 
+    # Handlers for direct and indirect encryption targets
+    decrypt_params = {
+        "output_folder": ctx.obj["output_folder"],
+        "iterations": ctx.obj["iterations"],
+        "salt": ctx.obj["salt"],
+    }
+
     if len(kwargs["targets"]) > 0:
-        decryptor.decrypt_targets(
-            output_folder=ctx.obj["output_folder"],
-            iterations=ctx.obj["iterations"],
-            salt=ctx.obj["salt"],
-            targets=kwargs["targets"],
-        )
+        decrypt_params.update(targets=kwargs["targets"])
+        decryptor.decrypt_targets(**decrypt_params)
     else:
-        decryptor.decrypt_targets(
-            output_folder=ctx.obj["output_folder"],
-            iterations=ctx.obj["iterations"],
-            salt=ctx.obj["salt"],
-        )
+        decryptor.decrypt_targets(**decrypt_params)
 
 
+# Entrypoint and context object
 def start():
     main(obj={})
 
